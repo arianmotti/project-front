9be74ef6e9129d86a9c59539df35ba4a2697a735
arianmotti <arianmottaghi2018@gmail.com>
2023-05-01 11:56:24 +0430

    finallly i fixed the answer divison colorrrrrrrrrr

--- src/app/disc/disc.component.ts
+++ src/app/disc/disc.component.ts
@@ -7,6 +7,7 @@ import { max } from 'rxjs';
   styleUrls: ['./disc.component.css']
 })
 export class DISCComponent implements OnInit {
+
   currentPage = 1;
   itemsPerPage = 1;
   result = ''
@@ -21,6 +22,15 @@ export class DISCComponent implements OnInit {
       answer2: 'اشتیاق',
       answer3:'راضی',
       answer4:'دیپلماتیک',
+      selectedD : false,
+      selectedI : false,
+      selectedS : false,
+      selectedC : false,
+      selected2D : false,
+      selected2I : false,
+      selected2S : false,
+      selected2C : false,
+      
       
     },
     {
@@ -30,6 +40,14 @@ export class DISCComponent implements OnInit {
       answer2: 'اشتیاق',
       answer3:'خوش خلق',
       answer4:'محتاط',
+      selectedD : false,
+      selectedI : false,
+      selectedS : false,
+      selectedC : false,
+      selected2D : false,
+      selected2I : false,
+      selected2S : false,
+      selected2C : false,
       
     },
     {
@@ -39,6 +57,14 @@ export class DISCComponent implements OnInit {
       answer2: 'دوستانه',
       answer3:'آرام ',
       answer4:'دقیق',
+      selectedD : false,
+      selectedI : false,
+      selectedS : false,
+      selectedC : false,
+      selected2D : false,
+      selected2I : false,
+      selected2S : false,
+      selected2C : false,
       
     },
     {
@@ -48,6 +74,14 @@ export class DISCComponent implements OnInit {
       answer2: 'پر حرف',
       answer3:' مرسوم',
       answer4:'کنترل شده',
+      selectedD : false,
+      selectedI : false,
+      selectedS : false,
+      selectedC : false,
+      selected2D : false,
+      selected2I : false,
+      selected2S : false,
+      selected2C : false,
       
     },
     {
@@ -57,6 +91,14 @@ export class DISCComponent implements OnInit {
       answer2: 'برونگرا',
       answer3:'معتدل',
       answer4:'با بصیرت',
+      selectedD : false,
+      selectedI : false,
+      selectedS : false,
+      selectedC : false,
+      selected2D : false,
+      selected2I : false,
+      selected2S : false,
+      selected2C : false,
       
     },
     {
@@ -66,6 +108,14 @@ export class DISCComponent implements OnInit {
       answer2: 'متقاعد کننده',
       answer3:'ملایم',
       answer4:'فروتن',
+      selectedD : false,
+      selectedI : false,
+      selectedS : false,
+      selectedC : false,
+      selected2D : false,
+      selected2I : false,
+      selected2S : false,
+      selected2C : false,
       
     },
     {
@@ -75,6 +125,14 @@ export class DISCComponent implements OnInit {
       answer2: 'رسا',
       answer3:'با وجدان',
       answer4:'نکته بین',
+      selectedD : false,
+      selectedI : false,
+      selectedS : false,
+      selectedC : false,
+      selected2D : false,
+      selected2I : false,
+      selected2S : false,
+      selected2C : false,
       
     },
     {
@@ -84,6 +142,14 @@ export class DISCComponent implements OnInit {
       answer2: 'بیانگر',
       answer3:'فروتن',
       answer4:'ناظر',
+      selectedD : false,
+      selectedI : false,
+      selectedS : false,
+      selectedC : false,
+      selected2D : false,
+      selected2I : false,
+      selected2S : false,
+      selected2C : false,
       
     },
     {
@@ -93,6 +159,14 @@ export class DISCComponent implements OnInit {
       answer2: 'جذاب',
       answer3:'تایید کننده',
       answer4:'با درایت',
+      selectedD : false,
+      selectedI : false,
+      selectedS : false,
+      selectedC : false,
+      selected2D : false,
+      selected2I : false,
+      selected2S : false,
+      selected2C : false,
       
     },
     {
@@ -102,6 +176,14 @@ export class DISCComponent implements OnInit {
       answer2: 'الهام بخش',
       answer3:'مطیع',
       answer4:'محافظه کار',
+      selectedD : false,
+      selectedI : false,
+      selectedS : false,
+      selectedC : false,
+      selected2D : false,
+      selected2I : false,
+      selected2S : false,
+      selected2C : false,
       
     },
     {
@@ -111,6 +193,14 @@ export class DISCComponent implements OnInit {
       answer2: 'شاد',
       answer3:'مکلف',
       answer4:'برنامه ریز',
+      selectedD : false,
+      selectedI : false,
+      selectedS : false,
+      selectedC : false,
+      selected2D : false,
+      selected2I : false,
+      selected2S : false,
+      selected2C : false,
       
     },
     {
@@ -120,6 +210,14 @@ export class DISCComponent implements OnInit {
       answer2: 'تحریک کننده',
       answer3:'مهربان',
       answer4:'با بصیرت',
+      selectedD : false,
+      selectedI : false,
+      selectedS : false,
+      selectedC : false,
+      selected2D : false,
+      selected2I : false,
+      selected2S : false,
+      selected2C : false,
       
     },
     {
@@ -129,6 +227,14 @@ export class DISCComponent implements OnInit {
       answer2: 'شادی آور',
       answer3:'محافظه کار',
       answer4:'باور به فردیت',
+      selectedD : false,
+      selectedI : false,
+      selectedS : false,
+      selectedC : false,
+      selected2D : false,
+      selected2I : false,
+      selected2S : false,
+      selected2C : false,
       
     },
     {
@@ -138,6 +244,14 @@ export class DISCComponent implements OnInit {
       answer2: 'بازیگوش',
       answer3:'مطیع',
       answer4:'منتقد',
+      selectedD : false,
+      selectedI : false,
+      selectedS : false,
+      selectedC : false,
+      selected2D : false,
+      selected2I : false,
+      selected2S : false,
+      selected2C : false,
       
     },
     {
@@ -147,6 +261,14 @@ export class DISCComponent implements OnInit {
       answer2: 'جذاب',
       answer3:'قابل حدس',
       answer4:'درون نگر',
+      selectedD : false,
+      selectedI : false,
+      selectedS : false,
+      selectedC : false,
+      selected2D : false,
+      selected2I : false,
+      selected2S : false,
+      selected2C : false,
       
     },
     {
@@ -156,6 +278,14 @@ export class DISCComponent implements OnInit {
       answer2: 'جذاب',
       answer3:'وفادار',
       answer4:'منطقی',
+      selectedD : false,
+      selectedI : false,
+      selectedS : false,
+      selectedC : false,
+      selected2D : false,
+      selected2I : false,
+      selected2S : false,
+      selected2C : false,
       
     },
     {
@@ -165,6 +295,14 @@ export class DISCComponent implements OnInit {
       answer2: 'دوستانه',
       answer3:'صبور',
       answer4:'صریح',
+      selectedD : false,
+      selectedI : false,
+      selectedS : false,
+      selectedC : false,
+      selected2D : false,
+      selected2I : false,
+      selected2S : false,
+      selected2C : false,
       
     },
     {
@@ -174,6 +312,14 @@ export class DISCComponent implements OnInit {
       answer2: 'روحیه ی بالا',
       answer3:'مایل',
       answer4:'کمال گرا',
+      selectedD : false,
+      selectedI : false,
+      selectedS : false,
+      selectedC : false,
+      selected2D : false,
+      selected2I : false,
+      selected2S : false,
+      selected2C : false,
       
     },
     {
@@ -183,6 +329,14 @@ export class DISCComponent implements OnInit {
       answer2: 'برونگرا',
       answer3:'دوست داشتنی',
       answer4:'ترسناک',
+      selectedD : false,
+      selectedI : false,
+      selectedS : false,
+      selectedC : false,
+      selected2D : false,
+      selected2I : false,
+      selected2S : false,
+      selected2C : false,
       
     },
     {
@@ -192,7 +346,14 @@ export class DISCComponent implements OnInit {
       answer2: 'مطمئن',
       answer3:'دلسوز',
       answer4:'بی طرف',
-      
+      selectedD : false,
+      selectedI : false,
+      selectedS : false,
+      selectedC : false,
+      selected2D : false,
+      selected2I : false,
+      selected2S : false,
+      selected2C : false,
     },
     {
       text : 'کدام یک از ویژگی های زیر را بیشتر از در خود می بینید؟',
@@ -201,7 +362,14 @@ export class DISCComponent implements OnInit {
       answer2: 'متحرک',
       answer3:'سخاوتمند',
       answer4:'منضبط',
-      
+      selectedD : false,
+      selectedI : false,
+      selectedS : false,
+      selectedC : false,
+      selected2D : false,
+      selected2I : false,
+      selected2S : false,
+      selected2C : false,
     },
     {
       text : 'کدام یک از ویژگی های زیر را بیشتر از در خود می بینید؟',
@@ -210,7 +378,14 @@ export class DISCComponent implements OnInit {
       answer2: 'تکانشی',
       answer3:'آسان گیر',
       answer4:'درون گرا',
-      
+      selectedD : false,
+      selectedI : false,
+      selectedS : false,
+      selectedC : false,
+      selected2D : false,
+      selected2I : false,
+      selected2S : false,
+      selected2C : false,
     },
     {
       text : 'کدام یک از ویژگی های زیر را بیشتر از در خود می بینید؟',
@@ -219,7 +394,14 @@ export class DISCComponent implements OnInit {
       answer2: 'همزن خوب(good mixer)',
       answer3:'ملایم',
       answer4:'فیلتر شده',
-      
+      selectedD : false,
+      selectedI : false,
+      selectedS : false,
+      selectedC : false,
+      selected2D : false,
+      selected2I : false,
+      selected2S : false,
+      selected2C : false,
     },
     {
       text : 'کدام یک از ویژگی های زیر را بیشتر از در خود می بینید؟',
@@ -228,7 +410,14 @@ export class DISCComponent implements OnInit {
       answer2: 'فریبنده',
       answer3:'راضی',
       answer4:'سازگار',
-      
+      selectedD : false,
+      selectedI : false,
+      selectedS : false,
+      selectedC : false,
+      selected2D : false,
+      selected2I : false,
+      selected2S : false,
+      selected2C : false,
     },
     {
       text : 'کدام یک از ویژگی های زیر را بیشتر از در خود می بینید؟',
@@ -237,7 +426,14 @@ export class DISCComponent implements OnInit {
       answer2: 'با نشاط',
       answer3:'تعاونی',
       answer4:'منظم',
-      
+      selectedD : false,
+      selectedI : false,
+      selectedS : false,
+      selectedC : false,
+      selected2D : false,
+      selected2I : false,
+      selected2S : false,
+      selected2C : false,
     },
     {
       text : 'کدام یک از ویژگی های زیر را بیشتر از در خود می بینید؟',
@@ -246,6 +442,14 @@ export class DISCComponent implements OnInit {
       answer2: 'شاد کننده',
       answer3:'یک نواخت',
       answer4:'دقیق',
+      selectedD : false,
+      selectedI : false,
+      selectedS : false,
+      selectedC : false,
+      selected2D : false,
+      selected2I : false,
+      selected2S : false,
+      selected2C : false,
       
     },
     {
@@ -255,7 +459,14 @@ export class DISCComponent implements OnInit {
       answer2: 'جذب کننده',
       answer3:'هم مسیر',
       answer4:'مراقب',
-      
+      selectedD : false,
+      selectedI : false,
+      selectedS : false,
+      selectedC : false,
+      selected2D : false,
+      selected2I : false,
+      selected2S : false,
+      selected2C : false,
     },
     {
       text : 'کدام یک از ویژگی های زیر را بیشتر از در خود می بینید؟',
@@ -264,7 +475,14 @@ export class DISCComponent implements OnInit {
       answer2: 'خوشبین',
       answer3:'مفید',
       answer4:'محترم',
-      
+      selectedD : false,
+      selectedI : false,
+      selectedS : false,
+      selectedC : false,
+      selected2D : false,
+      selected2I : false,
+      selected2S : false,
+      selected2C : false,
     },
    
    
@@ -275,11 +493,66 @@ export class DISCComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  setAnswer(index: number, value: string) {
+    this.questions[index].selectedD = false;
+    this.questions[index].selectedI = false;
+    this.questions[index].selectedS = false;
+    this.questions[index].selectedC = false;
+
+      
+
+    if (value=="D+") {
+      this.questions[index].selectedD = true;
+      
+    }
+    if (value==="I+") {
+      this.questions[index].selectedI = true;
+     
+    }
+    if (value==="S+") {
+      this.questions[index].selectedS = true;
+     
+    }
+    if (value==="C+") {
+      this.questions[index].selectedC = true;
+    }
+    
+    
+    this.answers[index] = value;
+  }
+  
+  setAnswer2(index: number, value: string) {
+    this.questions[index].selected2D = false;
+    this.questions[index].selected2I = false;
+    this.questions[index].selected2S = false;
+    this.questions[index].selected2C = false;
+    if (value=="D-") {
+      this.questions[index].selected2D = true;
+      
+    }
+    if (value==="I-") {
+      this.questions[index].selected2I = true;
+     
+    }
+    if (value==="S-") {
+      this.questions[index].selected2S = true;
+     
+    }
+    if (value==="C-") {
+      this.questions[index].selected2C = true;
+    }
+    
+    this.answers2[index] = value;
+  }
+  
+  
   getQuestionsForCurrentPage() {
     const startIndex = (this.currentPage - 1) * this.itemsPerPage;
     const endIndex = this.currentPage * this.itemsPerPage;
     return this.questions.slice(startIndex, endIndex);
   }
+  
+
 
   getTotalPages() {
     return Math.ceil(this.questions.length / this.itemsPerPage);
